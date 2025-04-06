2a89b6420ce9bc3d273fb2767a1ad5fdbe27c8c0
herrinjared <herrinjared@gmail.com>
2025-04-06 18:00:23 -0500

    Remove unused imports from emotes/models.py and emotes/admin.py

--- emotes/models.py
+++ emotes/models.py
@@ -1,6 +1,4 @@
 from django.db import models, transaction
-from django.core.validators import MinValueValidator, MaxValueValidator
-from django.conf import settings
 from PIL import Image
 from django.core.exceptions import ValidationError
 
